30f1f97f1c79be6765f2c9465676ee89bd36f45e
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-09-03 09:55:53 -0500

    Created centralized gravity script & adjusted player model

--- Assets/Scripts/CentralizedGravity.cs
+++ Assets/Scripts/CentralizedGravity.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class CentralizedGravity : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Transform target;
+    [SerializeField] int radius = 5;
+    [SerializeField] int forceAmount = 100;
+    [SerializeField] float gravity = 1f;
+    
+    Vector3 targetDirection;
+    Rigidbody rb;
+    float distance;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     void Start()
     {
+        //Physics.gravity = new Vector3(0, gravity, 0);
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        targetDirection = target.position - transform.position;
+        distance = targetDirection.magnitude;
+        targetDirection = targetDirection.normalized;
+
+        if (distance < radius)
+        {
+            rb.AddForce(targetDirection * forceAmount * Time.deltaTime);
+        }
+
+        Physics.gravity = new Vector3(targetDirection.x, targetDirection.y * forceAmount * Time.deltaTime, targetDirection.z);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
     }
 }
