4db0dcd72e6d1fc1d198f0052b1b4088e996f3c8
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-09-04 08:25:13 -0500

    Setting up more player animations

--- Assets/Scripts/StateMachine/Player/PlayerJumpingState.cs
+++ Assets/Scripts/StateMachine/Player/PlayerJumpingState.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class PlayerJumpingState : PlayerBaseState
 {
+    readonly int jumpHash = Animator.StringToHash("Jump");
+
+    const float crossFadeDuration = 0.1f;
+
     public PlayerJumpingState(PlayerStateMachine stateMachine) : base(stateMachine) { }
 
     public override void Enter()
     {
         stateMachine.Rigidbody.AddRelativeForce(new Vector3(0, stateMachine.JumpForce, 0), ForceMode.Impulse);
+
+        stateMachine.Animator.CrossFadeInFixedTime(jumpHash, crossFadeDuration);
     }
 
     public override void Tick(float deltaTime)
     {
-        Run(deltaTime);
-
         if (stateMachine.IsGrounded)
         {
             stateMachine.SwitchState(new PlayerRunningState(stateMachine));
