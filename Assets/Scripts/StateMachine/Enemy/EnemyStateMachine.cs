7dd95ea27812a2b2e063f0c305f11efc8f37e928
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-09-04 12:58:36 -0500

    Linked enemies to player health script

--- Assets/Scripts/StateMachine/Enemy/EnemyStateMachine.cs
+++ Assets/Scripts/StateMachine/Enemy/EnemyStateMachine.cs
@@ -4,5 +4,10 @@ using UnityEngine;
 
 public class EnemyStateMachine : StateMachine
 {
-    
+    [HideInInspector] public Health Player { get; private set; }
+
+    private void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
+    }
 }
