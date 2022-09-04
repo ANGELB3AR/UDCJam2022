9fc7dfede39a14e9ae844af624107454dceb6b46
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-09-04 13:07:49 -0500

    Created enemy chasing state

---
+++ Assets/Scripts/StateMachine/Enemy/EnemyChasingState.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChasingState : EnemyBaseState
+{
+    public EnemyChasingState(EnemyStateMachine stateMachine) : base(stateMachine) { }
+
+    public override void Enter()
+    {
+        
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        
+    }
+
+    public override void Exit()
+    {
+        
+    }
+}
--- Assets/Scripts/StateMachine/Enemy/EnemyStateMachine.cs
+++ Assets/Scripts/StateMachine/Enemy/EnemyStateMachine.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyStateMachine : StateMachine
 {
+    [field: SerializeField] public NavMeshAgent Agent { get; private set; }
+    [field: SerializeField] public Animator Animator { get; private set; }
     [HideInInspector] public Health Player { get; private set; }
 
     private void Start()
