d7871c1ebd52f820629e2a811d839688ea4cc6fd
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-09-05 06:42:51 -0500

    Created enemy idle state

---
+++ Assets/Scripts/StateMachine/Enemy/EnemyIdleState.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyIdleState : EnemyBaseState
+{
+    readonly int idleHash = Animator.StringToHash("Idle");
+
+    const float crossFadeDuration = 0.1f;
+
+    public EnemyIdleState(EnemyStateMachine stateMachine) : base(stateMachine) { }
+
+    public override void Enter()
+    {
+        stateMachine.Animator.CrossFadeInFixedTime(idleHash, crossFadeDuration);
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        
+    }
+
+    public override void Exit()
+    {
+        
+    }
+}
