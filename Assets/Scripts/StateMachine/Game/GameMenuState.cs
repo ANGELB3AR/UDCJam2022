45caae27572fcb1cfab14c7ceecf4fa5ea151ea9
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-09-07 06:26:57 -0500

    Created game states for game over, menu, playing, and paused

---
+++ Assets/Scripts/StateMachine/Game/GameMenuState.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenuState : GameBaseState
+{
+    public GameMenuState(GameStateMachine stateMachine) : base(stateMachine) { }
+
+    public override void Enter()
+    {
+
+    }
+
+    public override void Tick(float deltaTime)
+    {
+
+    }
+
+    public override void Exit()
+    {
+
+    }
+}
---
+++ Assets/Scripts/StateMachine/Game/GamePlayingState.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlayingState : GameBaseState
+{
+    public GamePlayingState(GameStateMachine stateMachine) : base(stateMachine) { }
+
+    public override void Enter()
+    {
+        
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        
+    }
+
+    public override void Exit()
+    {
+        
+    }
+}
