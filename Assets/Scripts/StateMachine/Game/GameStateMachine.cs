ac7dc49d36332ebc989597d89e0bff511aaaf91d
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-09-07 07:01:42 -0500

    Subscribed to player death event & used as trigger to transition to game over state

--- Assets/Scripts/StateMachine/Game/GameStateMachine.cs
+++ Assets/Scripts/StateMachine/Game/GameStateMachine.cs
@@ -7,8 +7,27 @@ public class GameStateMachine : StateMachine
     [field: SerializeField] public InputReader InputReader { get; private set; }
     [field: SerializeField] public LevelManager LevelManager { get; private set; }
 
-    private void Start()
+    [HideInInspector] public Health Player { get; private set; }
+
+    void OnEnable()
     {
+        Player.OnDeath += HandlePlayerDeath;
+    }
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
+
         SwitchState(new GamePlayingState(this));
+    }
+
+    void OnDisable()
+    {
+        Player.OnDeath -= HandlePlayerDeath;
+    }
+
+    void HandlePlayerDeath()
+    {
+        SwitchState(new GameOverState(this));
     }
 }
