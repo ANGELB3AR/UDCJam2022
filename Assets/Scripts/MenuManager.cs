c8bb866ec095a2165385287e93ff552424f42132
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2022-09-10 08:48:55 -0500

    Created menu manager and linked it to game manager via events

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] GameObject gameOverMenu;
+    [SerializeField] GameObject nextLevelMenu;
+    [SerializeField] GameObject pauseMenu;
+
+    void OnEnable()
+    {
+        GameManager.OnGameStateChanged += GameManagerOnGameStateChanged;
+    }
+
+    void OnDisable()
+    {
+        GameManager.OnGameStateChanged -= GameManagerOnGameStateChanged;
+    }
+
+    void GameManagerOnGameStateChanged(GameManager.GameState state)
+    {
+        gameOverMenu.SetActive(state == GameManager.GameState.Lose);
+        nextLevelMenu.SetActive(state == GameManager.GameState.Win);
+        pauseMenu.SetActive(state == GameManager.GameState.Paused);
+    }
+}
